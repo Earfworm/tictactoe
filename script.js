ba06b741e0ac68efeedf4ef905f85acb9cee4545
earfworm <deshawnreid2021@gmail.com>
2023-01-11 04:55:17 -0500

    maybe need to refactor the way stackoverflow suggets

--- script.js
+++ script.js
@@ -38,23 +38,108 @@ const playerMove = () => {
         cell.append(currentPlayer);
         playerTitle.innerHTML = "Player: 2";
         ticTacToeBoard.classList.toggle("player-turn");
-        cellsClicked.push(cell.getAttribute("id"));
-        if (
-          (cellsClicked[0] === winningMoves[0] &&
-            cellsClicked[1] === winningMoves[1] &&
-            cellsClicked[2] === winningMoves[2]) ||
-          (cellsClicked[0] === winningMoves[3] &&
-            cellsClicked[1] === winningMoves[4] &&
-            cellsClicked[2] === winningMoves[5]) ||
-          (cellsClicked[0] === winningMoves[6] &&
-            cellsClicked[1] === winningMoves[7] &&
-            cellsClicked[2] === winningMoves[8])
-        ) {
-          console.log("game over");
-        } else {
-          console.log(false);
-          console.log(cellsClicked, winningMoves);
+
+        cellsClicked.push(cell);
+        console.log(cellsClicked);
+        let gameOver = [];
+        for (let i = 0; i < cellsClicked.length; i++) {
+          if (
+            // horizontal 1st row
+            cellsClicked[i].getAttribute("id") === winningMoves[0] &&
+            cellsClicked[i].getAttribute("id") === winningMoves[1] &&
+            cellsClicked[i].getAttribute("id") === winningMoves[2]
+          ) {
+            gameOver.push(cellsClicked[i]);
+          } else if (
+            // horizontal 2nd row
+            cellsClicked[i].getAttribute("id") === winningMoves[3] &&
+            cellsClicked[i].getAttribute("id") === winningMoves[4] &&
+            cellsClicked[i].getAttribute("id") === winningMoves[5]
+          ) {
+            gameOver.push(cellsClicked[i]);
+          } else if (
+            // horizontal 3rd row
+            cellsClicked[i].getAttribute("id") === winningMoves[6] &&
+            cellsClicked[i].getAttribute("id") === winningMoves[7] &&
+            cellsClicked[i].getAttribute("id") === winningMoves[8]
+          ) {
+            gameOver.push(cellsClicked[i]);
+          } else if (
+            // vertical 1st row
+            cellsClicked[i].getAttribute("id") === winningMoves[0] &&
+            cellsClicked[i].getAttribute("id") === winningMoves[4] &&
+            cellsClicked[i].getAttribute("id") === winningMoves[6]
+          ) {
+            gameOver.push(cellsClicked[i]);
+          } else if (
+            // vertical 2nd row
+            cellsClicked[i].getAttribute("id") === winningMoves[1] &&
+            cellsClicked[i].getAttribute("id") === winningMoves[4] &&
+            cellsClicked[i].getAttribute("id") === winningMoves[7]
+          ) {
+            gameOver.push(cellsClicked[i]);
+          } else if (
+            // vertical 3rd row
+            cellsClicked[i].getAttribute("id") === winningMoves[2] &&
+            cellsClicked[i].getAttribute("id") === winningMoves[5] &&
+            cellsClicked[i].getAttribute("id") === winningMoves[8]
+          ) {
+            gameOver.push(cellsClicked[i]);
+          } else if (
+            // cells 1-5-9
+            cellsClicked[i].getAttribute("id") === winningMoves[0] &&
+            cellsClicked[i].getAttribute("id") === winningMoves[4] &&
+            cellsClicked[i].getAttribute("id") === winningMoves[8]
+          ) {
+            gameOver.push(cellsClicked[i]);
+          } else if (
+            // cells 3-5-7
+            cellsClicked[i].getAttribute("id") === winningMoves[2] &&
+            cellsClicked[i].getAttribute("id") === winningMoves[4] &&
+            cellsClicked[i].getAttribute("id") === winningMoves[6]
+          ) {
+            gameOver.push(cellsClicked[i]);
+          }
+          {
+          }
         }
+        // const clickedCell = cellsClicked.filter((cellClicked) => {
+        //   return (
+        //     // horizontal 1st row
+        //     ((cellClicked.getAttribute("id") === winningMoves[0] &&
+        //       cellClicked.getAttribute("id") === winningMoves[1] &&
+        //       cellClicked.getAttribute("id") === winningMoves[2]) ||
+        //       // horizontal 2nd row
+        //       cellClicked.getAttribute("id") === winningMoves[3]) &&
+        //     cellClicked.getAttribute("id") === winningMoves[4] &&
+        //     (cellClicked.getAttribute("id") === winningMoves[5] ||
+        //       // horizontal 3rd row
+        //       cellClicked.getAttribute("id") === winningMoves[6]) &&
+        //     cellClicked.getAttribute("id") === winningMoves[7] &&
+        //     (cellClicked.getAttribute("id") === winningMoves[8] ||
+        //       // vertical 1st row
+        //       cellClicked.getAttribute("id") === winningMoves[0]) &&
+        //     cellClicked.getAttribute("id") === winningMoves[4] &&
+        //     (cellClicked.getAttribute("id") === winningMoves[6] ||
+        //       // vertical 2nd row
+        //       cellClicked.getAttribute("id") === winningMoves[1]) &&
+        //     cellClicked.getAttribute("id") === winningMoves[5] &&
+        //     (cellClicked.getAttribute("id") === winningMoves[7] ||
+        //       // vertical 3rd row
+        //       cellClicked.getAttribute("id") === winningMoves[2]) &&
+        //     cellClicked.getAttribute("id") === winningMoves[5] &&
+        //     (cellClicked.getAttribute("id") === winningMoves[8] ||
+        //       // cells 1-5-9
+        //       cellClicked.getAttribute("id") === winningMoves[0]) &&
+        //     cellClicked.getAttribute("id") === winningMoves[4] &&
+        //     (cellClicked.getAttribute("id") === winningMoves[8] ||
+        //       // cells 3-5-7
+        //       cellClicked.getAttribute("id") === winningMoves[2]) &&
+        //     cellClicked.getAttribute("id") === winningMoves[4] &&
+        //     cellClicked.getAttribute("id") === winningMoves[6]
+        //   );
+        // });
+        console.log(gameOver);
       } else {
         let currentPlayer = document.createElement("p");
         currentPlayer.classList.add("player-2");
@@ -62,42 +147,6 @@ const playerMove = () => {
         cell.append(currentPlayer);
         playerTitle.innerHTML = "Player: 1";
         ticTacToeBoard.classList.toggle("player-turn");
-        if (
-          (cell.contains(currentPlayer) &&
-            cell.getAttribute("id") === "box-1" &&
-            cell.getAttribute("id") === "box-2" &&
-            cell.getAttribute("id") === "box-3") ||
-          (cell.contains(currentPlayer) &&
-            cell.getAttribute("id") === "box-4" &&
-            cell.getAttribute("id") === "box-5" &&
-            cell.getAttribute("id") === "box-6") ||
-          (cell.contains(currentPlayer) &&
-            cell.getAttribute("id") === "box-7" &&
-            cell.getAttribute("id") === "box-8" &&
-            cell.getAttribute("id") === "box-9") ||
-          (cell.contains(currentPlayer) &&
-            cell.getAttribute("id") === "box-1" &&
-            cell.getAttribute("id") === "box-4" &&
-            cell.getAttribute("id") === "box-7") ||
-          (cell.contains(currentPlayer) &&
-            cell.getAttribute("id") === "box-2" &&
-            cell.getAttribute("id") === "box-5" &&
-            cell.getAttribute("id") === "box-8") ||
-          (cell.contains(currentPlayer) &&
-            cell.getAttribute("id") === "box-3" &&
-            cell.getAttribute("id") === "box-6" &&
-            cell.getAttribute("id") === "box-9") ||
-          (cell.contains(currentPlayer) &&
-            cell.getAttribute("id") === "box-1" &&
-            cell.getAttribute("id") === "box-5" &&
-            cell.getAttribute("id") === "box-9") ||
-          (cell.contains(currentPlayer) &&
-            cell.getAttribute("id") === "box-3" &&
-            cell.getAttribute("id") === "box-5" &&
-            cell.getAttribute("id") === "box-7")
-        ) {
-          console.log("game over");
-        }
       }
     };
 
@@ -152,3 +201,20 @@ const startGame = () => {
 
 startGame();
 playerMove();
+
+// if (
+//   (cellsClicked[0] === winningMoves[0] &&
+//     cellsClicked[1] === winningMoves[1] &&
+//     cellsClicked[2] === winningMoves[2]) ||
+//   (cellsClicked[0] === winningMoves[3] &&
+//     cellsClicked[1] === winningMoves[4] &&
+//     cellsClicked[2] === winningMoves[5]) ||
+//   (cellsClicked[0] === winningMoves[6] &&
+//     cellsClicked[1] === winningMoves[7] &&
+//     cellsClicked[2] === winningMoves[8])
+// ) {
+//   console.log("game over");
+// } else {
+//   console.log(false);
+//   console.log(cellsClicked, winningMoves);
+// }
